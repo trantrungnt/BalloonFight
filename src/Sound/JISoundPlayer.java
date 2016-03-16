9c42dcd356eb863825a126fb7ea60e386fe82b47
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-16 14:37:01 +0700

    add library javazoom.jl to use load mp3 (effect sound in game)

---
+++ src/Sound/JISoundPlayer.java
@@ -0,0 +1,38 @@
+package Sound;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.*;
+/**
+ * Created by TrungNT on 3/16/2016.
+ */
+
+
+public class JISoundPlayer {
+    //dung thu vien JLayer javazoom.ji doc cac file mp3 dang effect cua Game
+
+    public void playmp3(String dir)
+    {
+        try {
+            File file = new File(dir);
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+
+
+            Player player = null;
+            try {
+                player = new Player(fis);
+                player.play();
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
