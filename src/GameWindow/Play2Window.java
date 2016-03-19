5a53a777720528edeed1b70a531ddab371d6ca32
HuongNguyen0706 <nguyenthuyhuong234@gmail.com>
2016-03-19 12:13:14 +0700

    [Player2Window]

--- src/GameWindow/Play2Window.java
+++ src/GameWindow/Play2Window.java
@@ -1,5 +1,11 @@
 package GameWindow;
 
+import GameObject.NewEnemy;
+import GameObject.NewEnemyManager;
+import GameObject.Obstacles.Obstacle;
+import GameObject.PlayManager;
+import GameObject.Player;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -8,16 +14,35 @@ import java.awt.event.KeyListener;
  * Created by Admin on 3/19/2016.
  */
 public class Play2Window extends GameWindow implements KeyListener {
+    public Play2Window play2Window;
     public Play2Window(){
-
-    }
-
-    public void draw(){
 
     }
 
     @Override
     public void draw(Graphics g) {
+        Play2WindowManager.getInstance().getCafe().draw(g);
+        Play2WindowManager.getInstance().getInk().draw(g);
+        Play2WindowManager.getInstance().getCloud().draw(g);
+        Play2WindowManager.getInstance().getMountain().draw(g);
+        for (Obstacle obstacleLand : PlayWindowManager.getInstance().getObstacleVectorLand()) {
+            obstacleLand.draw(g);
+        }
+
+        for (int i = 0; i < (PlayWindowManager.getInstance().getObstacleLake().size() - 1) / 2; i++) {
+            PlayWindowManager.getInstance().getObstacleLake().get(i).draw(g);
+        }
+        for (Player player : PlayManager.getInstance().getPlayerVector()) {
+            player.draw(g);
+        }
+
+        for (NewEnemy newEnemy : NewEnemyManager.getInstance().getNewEnemyVector()) {
+            newEnemy.draw(g);
+        }
+
+        for (int j = (PlayWindowManager.getInstance().getObstacleLake().size() - 1) / 2; j < PlayWindowManager.getInstance().getObstacleLake().size(); j++) {
+            PlayWindowManager.getInstance().getObstacleLake().get(j).draw(g);
+        }
 
     }
 
--- src/GameWindow/Play2WindowManager.java
+++ src/GameWindow/Play2WindowManager.java
@@ -1,9 +1,11 @@
 package GameWindow;
 
-import GameObject.Cafe;
-import GameObject.Ink;
+import GameObject.*;
+import GameObject.Obstacles.Island;
+import GameObject.Obstacles.Lake;
+import GameObject.Obstacles.Obstacle;
 
-import javax.sql.rowset.CachedRowSet;
+import java.util.Vector;
 
 /**
  * Created by Admin on 3/19/2016.
@@ -12,7 +14,60 @@ public class Play2WindowManager {
     private Play2Window play2Window;
     private Ink ink;
     private Cafe cafe;
+    private Mountain mountain;
+    private Cloud cloud;
+    private MenuButton menuButton;
+    private Vector<Obstacle> obstacleVector;
+    private Vector<Obstacle> obstacleLake;
 
+
+    private static Play2WindowManager ourInstance = new Play2WindowManager();
+
+    public static Play2WindowManager getInstance() {
+        return ourInstance;
+    }
+
+    private Play2WindowManager() {
+        this.reset();
+        play2Window = new Play2Window();
+    }
+
+    public void reset() {
+
+        obstacleVector = new Vector<>();
+        obstacleVector.add(new Island(900, 630));
+        obstacleVector.add(new Island(810, 630));
+        obstacleVector.add(new Island(55, 630));
+        obstacleVector.add(new Island(0, 630));
+
+        obstacleLake = new Vector<>();
+        obstacleLake.add(new Lake(150, 640));
+        obstacleLake.add(new Lake(450, 640));
+        obstacleLake.add(new Lake(0, 675));
+        obstacleLake.add(new Lake(640, 672));
+        obstacleLake.add(new Lake(200, 680));
+        obstacleLake.add(new Lake(450, 680));
+        obstacleLake.add(new Lake(0, 700));
+        obstacleLake.add(new Lake(200, 700));
+        obstacleLake.add(new Lake(450, 700));
+        obstacleLake.add(new Lake(640, 700));
+
+        mountain = new Mountain(0, 550);
+        cloud = new Cloud(502, 150);
+        menuButton = new MenuButton(900, 50);
+
+        ink = new Ink(100, 100);
+        cafe = new Cafe(200, 250);
+
+    }
+
+    public Play2Window getPlay2Window() {
+        return play2Window;
+    }
+
+    public void setPlay2Window(Play2Window play2Window) {
+        this.play2Window = play2Window;
+    }
 
     public Ink getInk() {
         return ink;
@@ -30,14 +85,44 @@ public class Play2WindowManager {
         this.cafe = cafe;
     }
 
-    public void reset(){
-        ink = new Ink(100, 100);
-        cafe = new Cafe(200, 250);
+    public Mountain getMountain() {
+        return mountain;
     }
-    public Play2WindowManager(){
-        this.reset();
-        play2Window = new Play2Window();
 
+    public void setMountain(Mountain mountain) {
+        this.mountain = mountain;
+    }
+
+    public Cloud getCloud() {
+        return cloud;
+    }
+
+    public void setCloud(Cloud cloud) {
+        this.cloud = cloud;
+    }
+
+    public MenuButton getMenuButton() {
+        return menuButton;
+    }
+
+    public void setMenuButton(MenuButton menuButton) {
+        this.menuButton = menuButton;
+    }
+
+    public Vector<Obstacle> getObstacleVector() {
+        return obstacleVector;
+    }
+
+    public void setObstacleVector(Vector<Obstacle> obstacleVector) {
+        this.obstacleVector = obstacleVector;
+    }
+
+    public Vector<Obstacle> getObstacleLake() {
+        return obstacleLake;
+    }
+
+    public void setObstacleLake(Vector<Obstacle> obstacleLake) {
+        this.obstacleLake = obstacleLake;
     }
 
 
